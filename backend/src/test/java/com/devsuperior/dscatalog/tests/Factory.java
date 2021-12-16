69cafeec7cff16080b27f68d103a7ad072404197
sergio2305 <sergio230564@gmail.com>
2021-12-15 22:46:50 -0300

    Tdd com SpringBoot

---
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+public class Factory {
+	
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20t03:00:00Z"));
+		product.getCategories().add(new Category(2L, "Electronic"));
+		return product;		
+	}
+
+	public static ProductDTO createProduvtDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+		
+	}
+}
